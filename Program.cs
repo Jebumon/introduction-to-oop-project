20537d0e8f6bf88d9b5b64200df0fbf279066d92
jebumon <jebukthomas@gmail.com>
2022-09-02 01:20:52 +0100

    Created Bird Class and pushing to main branch

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IntroductionTo_OOPs_Project.Models;
+
+Bird myBird = new Bird("Tweety", "Yellow");
+myBird.Speak();
+myBird.Fly();
+
